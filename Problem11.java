e9607ff50be9d43b818cc43296b87f8436d82a02
Ankit Raj <ankitraj4052@gmail.com>
2025-05-08 22:50:06 +0530

    Problem11

--- Problem11.java
+++ Problem11.java
@@ -1,3 +1,7 @@
+/*  Problem:-- Ron wishes to renovate his barn. He thinks that creating fences and keeping the animals separate will help him feed the animals more easily and prevent them from getting mixed.
+For this, he plans to divide the area into three parts. He wishes to have a square area for the chickens, a circular area for the ducks, and a rectangular area for the cows.
+Write a program to help Ron calculate the area of each fence. */
+
 import java.util.*;
 
 public class Problem11 {
