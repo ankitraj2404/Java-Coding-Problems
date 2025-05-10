ac049703e372f45ad11d7a5421b1f17c38e4692f
Ankit Raj <ankitraj4052@gmail.com>
2025-05-10 12:45:00 +0530

    Pattern program

---
+++ Problem24.java
@@ -0,0 +1,34 @@
+
+/*Program to print the  following pattern
+
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15*/
+
+import java.util.*;
+
+public class Problem24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("Enter the no of rows: ");
+            int row = sc.nextInt();
+
+            int counter = 0;
+            for (int i = 1; i <= row; i++) {
+                for (int j = 1; j <= i; j++) {
+                    counter++;
+                    System.out.print(counter + " ");
+                }
+                System.out.println();
+            }
+            sc.close();
+
+        } catch (InputMismatchException e) {
+            System.out.println("Enter valid values");
+        }
+    }
+}
