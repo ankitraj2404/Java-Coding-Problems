31382ea817e08387fff64c63081b3c0f0b46ebd5
Ankit Raj <ankitraj4052@gmail.com>
2025-05-10 12:10:19 +0530

    Pattern-Program

---
+++ problem22.java
@@ -0,0 +1,24 @@
+//Program to print the rectangle pattern
+
+import java.util.*;
+
+public class problem22 {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the no of rows and columns: ");
+            int row = sc.nextInt();
+            int col = sc.nextInt();
+
+            for (int i = 1; i <= row; i++) {
+                for (int j = 1; j <= col; j++) {
+                    System.out.print(j);
+                }
+                System.out.println();
+            }
+            sc.close();
+        } catch (InputMismatchException e) {
+            System.out.println("Enter only valid values");
+        }
+    }
+}
