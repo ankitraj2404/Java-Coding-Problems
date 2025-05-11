3373757be1f301e5c17f01d310d0465ccbbec964
Ankit Raj <ankitraj4052@gmail.com>
2025-05-12 00:16:07 +0530

    Pattern Program

---
+++ Problem32.java
@@ -0,0 +1,37 @@
+/*Program to print the following pattern
+
+    1 
+   1 2
+  1 2 3
+ 1 2 3 4
+1 2 3 4 5
+
+*/
+
+import java.util.*;
+
+public class Problem32 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("Enter the no of rows: ");
+            int row = sc.nextInt();
+
+            for (int i = 1; i <= row; i++) {
+                int count = 1;
+                for (int j = 1; j <= row; j++) {
+                    if (i + j <= row) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print(count + " ");
+                        count++;
+                    }
+                }
+                System.out.println();
+            }
+        } catch (InputMismatchException e) {
+            System.out.println();
+        }
+    }
+}
