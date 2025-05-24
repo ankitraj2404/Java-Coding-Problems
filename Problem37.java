b2f986b598a44e3640b6cd1a4c7dea440d43d011
Ankit Raj <ankitraj4052@gmail.com>
2025-05-24 13:57:20 +0530

    Initial commit

--- Problem37.java
+++ Problem37.java
@@ -4,8 +4,7 @@ import java.util.*;
 
 public class Problem37 {
 
-    static void findGcd(int num1, int num2) {
-        int temp1 = num1, temp2 = num2;
+    static int findGcd(int num1, int num2) {
         while (num1 != num2) {
             if (num1 > num2) {
                 num1 = num1 - num2;
@@ -13,18 +12,27 @@ public class Problem37 {
                 num2 = num2 - num1;
             }
         }
-        System.out.println("GCD of " + temp1 + " and " + temp2 + " is: " + num1);
+        return num1;
+    }
+
+    static void findLcm(int num1, int num2) {
+        int gcd = findGcd(num1, num2);
+        int lcm = num1 * num2 / gcd;
+
+        System.out.println("LCM of " + num1 + " and " + num2 + " is: " + lcm);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         try {
-            System.out.println("Enter two numbers: ");
+            System.out.println("Enter two numbers to find LCM and GCD: ");
             int num1 = sc.nextInt();
             int num2 = sc.nextInt();
 
-            findGcd(num1, num2);
+            int gcd = findGcd(num1, num2);
+            System.out.println("GCD of " + num1 + " and " + num2 + " is: " + gcd);
+            findLcm(num1, num2);
         } catch (InputMismatchException e) {
             System.out.println("Invalid Input");
         } finally {
