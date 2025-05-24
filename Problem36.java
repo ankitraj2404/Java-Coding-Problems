d336e0f14583645d3fcd14846440768795007e99
Ankit Raj <ankitraj4052@gmail.com>
2025-05-24 13:29:19 +0530

    Initial commit

--- Problem36.java
+++ Problem36.java
@@ -3,24 +3,29 @@
 import java.util.*;
 
 public class Problem36 {
+
+    static void checkPrime(int num) {
+        int count = 0;
+
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+        if (count > 2) {
+            System.out.println(num + " is not a prime number");
+        } else {
+            System.out.println(num + " is a prime number");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         try {
             System.out.println("Enter the number to check: ");
             int num = sc.nextInt();
-            int count = 0;
-
-            for (int i = 1; i <= num; i++) {
-                if (num % i == 0) {
-                    count++;
-                }
-            }
-            if (count > 2) {
-                System.out.println(num + " is not a prime number");
-            } else {
-                System.out.println(num + " is a prime number");
-            }
+            checkPrime(num);
         } catch (InputMismatchException e) {
             System.out.println("Invalid Input");
         } finally {
