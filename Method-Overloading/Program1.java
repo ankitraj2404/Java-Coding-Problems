4dc2eaf88e2f09d07d43a27bd90c335b20963ea7
Ankit Raj <ankitraj4052@gmail.com>
2025-05-24 15:21:21 +0530

    Initial commit

--- Method-Overloading/Program1.java
+++ Method-Overloading/Program1.java
@@ -1,25 +1,59 @@
+//Program to find the area of rectangle, circle and cuboid
+
+import java.util.*;
 
 public class Program1 {
 
-    static int findMax(int x, int y) {
-        return x > y ? x : y;
+    static void findArea(int length, int breadth) {
+        int area = length * breadth;
+        System.out.println("Area of the rectangle is: " + area);
     }
 
-    static int findMax(int x, int y, int z) {
-        if (x > y && x > z) {
-            return x;
-        } else if (y > z) {
-            return y;
-        } else {
-            return z;
-        }
+    static void findArea(int length, int width, int height) {
+        int area = 2 * (length * width + length * height + height * width);
+        System.out.println("Area of the cuboid is: " + area);
+    }
+
+    static void findArea(float radius) {
+        float area = (float) Math.PI * (radius * radius);
+        System.out.printf("Area of the circle is: %.2f\n", area);
+
     }
 
     public static void main(String[] args) {
-        int method1 = findMax(25, 45);
-        int method2 = findMax(25, 45, 55);
-        System.out.print(method1);
-        System.out.print(method2);
-    }
+        Scanner sc = new Scanner(System.in);
 
+        try {
+            System.out.println("Enter the shape for finding area(1 for Rectangle, 2 for Circle, 3 for Cuboid)");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter the length and breadth of the rectangle: ");
+                    int length = sc.nextInt();
+                    int breadth = sc.nextInt();
+                    findArea(length, breadth);
+                    break;
+                case 2:
+                    System.out.println("Enter the radius of the circle: ");
+                    float radius = sc.nextFloat();
+                    findArea(radius);
+                    break;
+                case 3:
+                    System.err.println("Enter the length, width and height of the cuboid: ");
+                    int len = sc.nextInt();
+                    int width = sc.nextInt();
+                    int height = sc.nextInt();
+                    findArea(len, width, height);
+                    break;
+                default:
+                    System.out.println("Invalid input");
+                    break;
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter numbers only.");
+        } finally {
+            sc.close();
+        }
+    }
 }
