434a9a936a6c7eef6b7ae2ec73a70f90fd6b86fd
Ankit Raj <ankitraj4052@gmail.com>
2025-07-30 20:06:45 +0530

    Exception Handling

---
+++ Exception-Handling/Problem4.java
@@ -0,0 +1,15 @@
+//Write a Java program where an exception is thrown inside a nested try block and re-thrown to be caught in an outer catch block.
+public class Problem4 {
+    public static void main(String[] args) {
+        try {
+            try {
+                throw new NullPointerException("Inner Null Value");
+            } catch (NullPointerException e) {
+                System.out.println("inner catch: " + e.getMessage());
+                throw e;
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Outer catch: " + e.getMessage());
+        }
+    }
+}
