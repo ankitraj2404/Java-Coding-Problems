a1e7c8bdb006fdeea2995bf34ff2831a03b6faa8
Ankit Raj <ankitraj4052@gmail.com>
2025-07-30 18:38:43 +0530

    Exception Handling

---
+++ Exception-Handling/Problem1.java
@@ -0,0 +1,18 @@
+
+//Write a Java program that throws an exception and catch it using a try-catch block.
+import java.util.Scanner;
+
+public class Problem1 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter two numbers to perform division: ");
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            int result = a / b;
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Enter valid denominator " + e);
+        }
+    }
+}
