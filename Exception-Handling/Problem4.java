8757f44fbb4207fa66341612266189582939506e
Ankit Raj <ankitraj4052@gmail.com>
2025-07-31 23:33:56 +0530

    Exception Handling

--- Exception-Handling/Problem4.java
+++ Exception-Handling/Problem4.java
@@ -1,4 +1,5 @@
 // Write a Java program where an exception is thrown inside a nested try block and re-thrown to be caught in an outer catch block.
+
 public class Problem4 {
     public static void main(String[] args) {
         try {
