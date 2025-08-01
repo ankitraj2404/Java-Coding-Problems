4edf0b7cbf7db49b37c18c3dc7c3580dc10da194
Ankit Raj <ankitraj4052@gmail.com>
2025-08-01 22:22:34 +0530

    File Created

---
+++ Java-IO-Streams/Problem4.java
@@ -0,0 +1,36 @@
+// Write a java program to copy one the content from one file to another
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Problem4 {
+    public static void main(String[] args) {
+        try (FileInputStream fis = new FileInputStream("C:/MyText/Output.txt");
+                FileOutputStream fos = new FileOutputStream("C:/MyText/Copy.txt");
+                FileInputStream fis1 = new FileInputStream("C:/MyText/Copy.txt")) {
+
+            int b;
+
+            while ((b = fis.read()) != -1) {
+
+                if (b >= 65 && b <= 90)
+                    fos.write(b + 32);
+            }
+
+            byte[] arr = new byte[fis1.available()];
+
+            fis1.read(arr);
+
+            String str = new String(arr);
+
+            System.out.println("The copied content is: " + str);
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
