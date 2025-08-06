d253b65d52b1fcaac6abea239b7781fb1810e4b0
Ankit Raj <ankitraj4052@gmail.com>
2025-08-07 00:48:29 +0530

    ArrayList

---
+++ Collections/List/ArrayList/Problem3.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Problem3 {
+    public static void main(String[] args) {
+
+        ArrayList<String> colors = new ArrayList<>(List.of("Red", "Blue", "Green", "Light Green", "Light Red"));
+        System.out.println("original List: " + colors);
+
+        String subString = "Light";
+
+        ArrayList<String> newList = colors.stream() // One way by using Streams
+                .filter((c1) -> {
+                    if (!c1.contains(subString)) {
+                        return true;
+                    }
+                    return false;
+                })
+                .collect(Collectors.toCollection(ArrayList::new));
+        System.out.println("After removal of " + subString + " from the list by using Streams: " + newList);
+
+        colors.removeIf(c -> c.contains(subString)); // Direct from the list
+        System.out.println("After removal of " + subString + " from the list: " + colors);
+    }
+}
