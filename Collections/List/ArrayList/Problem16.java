feaafaba2e1577f5715f5b8f984fff356ea73321
Ankit Raj <ankitraj4052@gmail.com>
2025-08-09 23:53:09 +0530

    ArrayList

--- Collections/List/ArrayList/Problem16.java
+++ Collections/List/ArrayList/Problem16.java
@@ -1,7 +1,6 @@
 //Write a Java program to retrieve an element (at a specified index) from a given array list.
 
 import java.util.ArrayList;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Problem16 {
