3d96cee97502fdd8dac829469880b5de64451742
Ankit Raj <ankitraj4052@gmail.com>
2025-08-09 23:52:52 +0530

    ArrayList

--- Collections/List/ArrayList/Problem16.java
+++ Collections/List/ArrayList/Problem16.java
@@ -1,6 +1,7 @@
 //Write a Java program to retrieve an element (at a specified index) from a given array list.
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Problem16 {
@@ -21,8 +22,13 @@ public class Problem16 {
             System.out.println("Enter the index between 0 to " + size + " you want to access: ");
             int index = sc.nextInt();
 
-            int element = list.get(index);
-            System.out.println("The element at index " + index + " is: " + element);
+            if (index < 0 || index >= size) {
+                System.out.println("Please enter valid index");
+            } else {
+                int element = list.get(index);
+                System.out.println("The element at index " + index + " is: " + element);
+            }
+
         } catch (Exception e) {
             System.out.println(e.getClass().getSimpleName());
         }
