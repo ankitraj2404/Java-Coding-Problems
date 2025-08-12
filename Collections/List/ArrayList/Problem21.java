130c62df49792c22b00da05220e80d268718fff5
Ankit Raj <ankitraj4052@gmail.com>
2025-08-12 20:32:28 +0530

    ArrayList

---
+++ Collections/List/ArrayList/Problem21.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Problem21 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+
+            System.out.println("How many elements do you want to enter?");
+            int size = sc.nextInt();
+
+            ArrayList<Integer> list = new ArrayList<>();
+
+            System.out.println("Enter elements: ");
+            for (int i = 0; i < size; i++) {
+                int num = sc.nextInt();
+                list.add(num);
+            }
+
+            System.out.println("Orgininal list: " + list);
+
+            System.out.println("Enter the element that you want to remove from the list: ");
+            int valueToRemove = sc.nextInt();
+
+            List<Integer> modifiedList = list.stream()
+                    .filter(i -> i != valueToRemove)
+                    .collect(Collectors.toList());
+            System.out.println("List after removing all occurences of " + valueToRemove + ": " + modifiedList);
+
+        } catch (Exception e) {
+            System.out.println(e.getClass().getSimpleName());
+        }
+    }
+}
