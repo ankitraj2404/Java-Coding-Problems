a37cb33a3ed2176d6b418b305626d1fcf03db988
Ankit Raj <ankitraj4052@gmail.com>
2025-08-19 23:56:55 +0530

    HashSet

---
+++ Collections/Set/HashSet/Problem14.java
@@ -0,0 +1,28 @@
+//Write a Java program to empty an hash set.
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Problem14 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+
+            System.out.println("Enter elements in the hash set(Enter 0 to stop): ");
+            HashSet<Integer> set = new HashSet<>();
+            int value;
+            do {
+                value = sc.nextInt();
+                if (value != 0) {
+                    set.add(value);
+                }
+            } while (value != 0);
+
+            set.clear();
+
+            System.out.println("Is set empty ? Ans:-" + set.isEmpty());
+
+        } catch (Exception e) {
+            System.out.println(e.getClass().getSimpleName());
+        }
+    }
+}
