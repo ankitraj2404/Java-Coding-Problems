002f456a1e665ba3ddb8cc9377239d849440439b
Ankit Raj <ankitraj4052@gmail.com>
2025-08-25 22:48:18 +0530

    OOPS

--- Problem63.java
+++ Problem63.java
@@ -1,4 +1,4 @@
-// Write a Java program to create a class called "Restaurant" with attributes for menu items, prices, and ratings, and methods to add and remove items, and to calculate average rating.
+//Write a Java program to create a class called "Restaurant" with attributes for menu items, prices, and ratings, and methods to add and remove items, and to calculate average rating.
 
 import java.util.HashMap;
 import java.util.Map;
