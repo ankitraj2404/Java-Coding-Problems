cb2c563c5edb7ad14383c28b7095e2ccae46f6f9
Ankit Raj <ankitraj4052@gmail.com>
2025-09-06 23:46:44 +0530

    Completed

---
+++ Array-Problems/Problem42.java
@@ -0,0 +1,39 @@
+//Write a Java program to reverse a section of an array between two given indices.
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Problem42 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the size of the array: ");
+            int size = sc.nextInt();
+
+            int[] array = new int[size];
+            System.out.println("Enter elements: ");
+            for (int i = 0; i < size; i++) {
+                array[i] = sc.nextInt();
+            }
+            System.out.println("Original Array: " + Arrays.toString(array));
+
+            System.out.println("Enter the range of index value to reverse between 0 and " + (size - 1) + ": ");
+            int startingIndex = sc.nextInt();
+            int endingIndex = sc.nextInt();
+
+            if (startingIndex < 0 || endingIndex >= size) {
+                return;
+            }
+            while (startingIndex < endingIndex) {
+                int temp = array[startingIndex];
+                array[startingIndex] = array[endingIndex];
+                array[endingIndex] = temp;
+                startingIndex++;
+                endingIndex--;
+            }
+            System.out.println("Original Array: " + Arrays.toString(array));
+
+        } catch (Exception e) {
+            System.out.println(e.getClass().getSimpleName());
+        }
+    }
+}
