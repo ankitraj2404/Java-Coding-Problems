9868911d417c8a6046722a3ab523a6a761baf35a
Ankit Raj <ankitraj4052@gmail.com>
2025-10-21 22:39:17 +0530

    Completed

---
+++ Collections/Problem1.java
@@ -0,0 +1,19 @@
+// Write a Java program to create an array list, add some colors (strings) and print out the collection.
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Problem1 {
+    public static void main(String[] args) {
+
+        List<String> colors = new ArrayList<>(List.of("Red", "Blue", "Green", "Violet", "Indigo"));
+
+        colors.forEach(System.out::println); // one way
+
+        Iterator<String> it = colors.iterator();// by using iterator interface
+
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+}
